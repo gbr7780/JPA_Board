f0cde95952cb69ba0f7982e614f872550ef5e380
jongha <gbr369369@naver.com>
2024-03-17 17:07:28 +0900

    feat: 스프링 프로젝트 생성

---
+++ src/test/java/com/JPA_Board/JpaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JPA_Board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
